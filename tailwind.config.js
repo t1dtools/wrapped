9e3408b996c85fe78a33c41b62c88823506d8979
Andreas Stokholm <andreas@stokholm.me>
2023-02-22 22:44:13 +0100

    enable header animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "text-bg": 'text-bg 15s ease infinite',
+      },
+      keyframes: {
+        "text-bg": {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '25%': {backgroundPosition: '50% 50%'},
+          '50%': { backgroundPosition: '100% 50%' },
+          '75%': { backgroundPosition: '25% 25%' },
+        },
+      },
+    },
   },
   plugins: [],
 }
